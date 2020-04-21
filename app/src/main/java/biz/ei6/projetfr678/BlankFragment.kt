454dceb7374529ca6877432037941e930c282dcd
Fabien <fabien.brissonneau@gmail.com>
2020-04-21 17:57:02 +0200

    Pour FR_678_02_02

---
+++ app/src/main/java/biz/ei6/projetfr678/BlankFragment.kt
@@ -0,0 +1,45 @@
+package biz.ei6.projetfr678
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_blank.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment(val blankFragmentListener: BlankFragmentListener) : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val ret =  inflater.inflate(R.layout.fragment_blank, container, false)
+
+        ret.blank_next.setOnClickListener { blankFragmentListener.next() }
+
+        return ret
+    }
+
+
+
+    interface BlankFragmentListener {
+        fun next()
+    }
+}
