a1d6cb88d95220f82a26172b34b487139cec53f4
Fabien Brissonneau <fabien.brissonneau@gmail.com>
2020-04-23 11:41:10 +0200

    lifecycle owners and observers

--- app/src/main/java/biz/ei6/projetfr678/FirstFragment.kt
+++ app/src/main/java/biz/ei6/projetfr678/FirstFragment.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_first.*
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import biz.ei6.projetfr678.databinding.FragmentFirstBinding
+import biz.ei6.projetfr678.BR.mainViewModele
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -20,7 +21,7 @@ class FirstFragment : Fragment() {
 
     val mViewModele : MainViewModele by activityViewModels<MainViewModele>()
 
-    var binding : FirstFragmentBinding?
+    lateinit var binding : FragmentFirstBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -28,8 +29,8 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
        // return inflater.inflate(R.layout.fragment_first, container, false)
-        val binding =  DataBindingUtil.inflate(R.layout.fragment_first, container, false)
-        binding.setLifecycleOwner(this)
+        binding =  DataBindingUtil.inflate<FragmentFirstBinding>(inflater, R.layout.fragment_first, container, false)
+        binding.lifecycleOwner = this
         return binding.getRoot()
     }
 
@@ -43,27 +44,30 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 
         }
+
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        main_first_date.append(mViewModele.date.format(DateTimeFormatter.ISO_LOCAL_DATE))
+            /*   main_first_date.append(mViewModele.date.format(DateTimeFormatter.ISO_LOCAL_DATE))
         main_first_arrivee_km.append(mViewModele.arriveeKM.toString())
         main_first_arrivee_location.append(mViewModele.arriveeLocation)
         main_first_depart_km.append(mViewModele.departKM.toString())
         main_first_depart_location.append(mViewModele.departLocation)
+*/
 
-
-
+        binding.setVariable(mainViewModele,mViewModele)
     }
 
     private fun valider() {
-        mViewModele.date = LocalDate.parse(main_first_date.text,DateTimeFormatter.ISO_LOCAL_DATE)
+    /*    mViewModele.date = LocalDate.parse(main_first_date.text,DateTimeFormatter.ISO_LOCAL_DATE)
         mViewModele.departKM = Integer.parseInt(main_first_depart_km.text.toString())
         mViewModele.arriveeKM = Integer.parseInt(main_first_arrivee_km.text.toString())
 
         mViewModele.departLocation = main_first_depart_location.text.toString()
-        mViewModele.arriveeLocation = main_first_arrivee_location.text.toString()
+        mViewModele.arriveeLocation = main_first_arrivee_location.text.toString()*/
+
     }
 }
--- app/src/main/java/biz/ei6/projetfr678/MainViewModele.kt
+++ app/src/main/java/biz/ei6/projetfr678/MainViewModele.kt
@@ -1,24 +1,46 @@
 package biz.ei6.projetfr678
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainViewModele : ViewModel() {
+    val TAG = "MAINVIEWMODELE"
 
     var date : LocalDate = LocalDate.now()
 
+    var dateTxt : String = ""
+        get() = date.format(DateTimeFormatter.ISO_LOCAL_DATE)
+        set(value) { field = value; date = LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE)}
+
     var departKM : Int  = 0
         set(value) { field = value; kilometres.value =  arriveeKM-value }
+
+    var departKMTxt : String =""
+        get() = ""+departKM
+        set(value) {
+            field = value; if(value.isNotBlank()) departKM = Integer.parseInt(value) else departKM=0}
 
     var departLocation : String = ""
 
     var arriveeKM : Int = 0
         set(value) { field = value; kilometres.value =  value-departKM }
 
+    var arriveeKMTxt : String =""
+        get() = ""+arriveeKM
+        set(value) { field = value; if(value.isNotBlank()) arriveeKM = Integer.parseInt(value) else arriveeKM = 0 }
+
     var arriveeLocation : String = ""
 
 
     var kilometres = MutableLiveData<Int>()
 
+
+    fun enregistre() {
+        Log.d(TAG, "Ici on enregistrera les données ...")
+    }
 }
